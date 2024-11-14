0aa704bf7b6e88b5ad3c7a2d2f30d9299a1d6476
Ethan Cao <1677035769@qq.com>
2024-11-14 21:07:06 +0800

    24/11/14 21:07

--- recv.sh
+++ recv.sh
@@ -3,8 +3,11 @@
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 cd "$SCRIPT_DIR" || exit
 
-MODE="${1}"
-OUTPUT="${2}"
+MODE="${1}"   # 传输模式 (匹配send.sh)
+OUTPUT="${2}" # 输出文件
+
+# 示例用法: ./recv.sh GBN data/10M.recv
+# 示例用法: ./recv.sh SR  data/100M.recv
 
 python3 code/Server.py \
     -mode "$MODE" \
--- send.sh
+++ send.sh
@@ -3,20 +3,30 @@
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 cd "$SCRIPT_DIR" || exit
 
-MODE="${1}"
-INPUT="${2}"
-LOSS="${3}"
-DELAY="${4}"
+MODE="${1}"  # 传输模式
+VEGAS="${2}" # 窗口是否基于延迟
+INPUT="${3}" # 输入文件
+LOSS="${4}"  # 丢包率
+DELAY="${5}" # 延时
+
+# 示例用法: ./send.sh GBN False data/10M.send  0.01 10ms
+# 示例用法: ./send.sh SR  True  data/100M.send 0.001 1ms
 
 
-sudo tc qdisc del dev lo root
-sudo tc qdisc add dev lo root netem loss "$LOSS" delay "$DELAY"
-sudo tc qdisc show dev lo root
+# 本地模拟测试
+# SERVER=127.0.0.1
+# sudo tc qdisc del dev lo root
+# sudo tc qdisc add dev lo root netem loss "$LOSS" delay "$DELAY"
+# sudo tc qdisc show dev lo root
+
+# 真实服务器测试
+SERVER=47.254.22.72
+
 
 python3 code/Client.py \
-    -mode "$MODE" \
-    -vegas True \
-    -host 127.0.0.1 \
-    -port 12340 \
-    -input "$INPUT" \
-    -mss 1400
+-mode "$MODE" \
+-vegas "$VEGAS" \
+-host "$SERVER" \
+-port 12340 \
+-input "$INPUT" \
+-mss 1400
