c799c680a731b74ccee6e4dbaaa0870dbddf49cc
Ethan Cao <1677035769@qq.com>
2024-11-14 04:08:18 +0000

    24/11/14 12:07

---
+++ Send/SR_send.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+cd "$SCRIPT_DIR" || exit
+
+INPUT="data/send.gz"
+# INPUT="data/send.jpg"
+
+python3 SR_client.py \
+    -host 127.0.0.1 \
+    -port 12345 \
+    -input "$INPUT" \
+    -mss 1400 \
+    -window 64 \
+    -time 0.5 \
+    -loss 0.00 \
+    -corrupt 0.00
